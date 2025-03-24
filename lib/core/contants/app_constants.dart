ab792a610b0c5ef60be730d6ef2ac4cca05103f2
franco9652 <francodebuono9652@gmail.com>
2025-03-24 19:52:15 -0300

    pasamos a zea

--- lib/core/contants/app_constants.dart
+++ lib/core/contants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = "http://10.0.2.2:8080";
+  static const String baseUrl = "https://crmdbsoft.zeabur.app";
   static const String loginEndpoint = "$baseUrl/login";
   static const String registerEndpoint = "$baseUrl/auth/register";
   static const String customerCreateEndpoint = "$baseUrl/customerCreate";
--- lib/features/budgets/data/budget_data_source.dart
+++ lib/features/budgets/data/budget_data_source.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:crm_app_dv/core/contants/app_constants.dart';
 import 'package:crm_app_dv/models/budget_model.dart';
